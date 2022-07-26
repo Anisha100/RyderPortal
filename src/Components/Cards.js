51b024cbd05421c512acd227e2624e363d9dfdb5
Anisha100 <87559560+Anisha100@users.noreply.github.com>
2022-07-26 15:38:50 +0530

    Add files via upload

---
+++ src/Components/Cards.js
@@ -0,0 +1,122 @@
+import React from 'react';
+import aws from '../assets/aws.png'
+import node from '../assets/node.png'
+import mongo from '../assets/mongo.png'
+
+const Cards = () => {
+  return (
+    <div className=' m-w-[1000px] py-5   '>
+   <div className=' grid md:grid-cols-4 '>
+   <div className='w-64 shadow-md flex flex-col p-4 my-4   duration-300 '>
+   <img className='w-20 mx-auto mt-[-3rem] bg-white' src={aws} alt="/" />
+
+    <h2 className=' font-bold text-center '>Leslie Alexander</h2>
+    <p className='text-center text-gray-300 font-bold'>Role</p>
+    <div className='text-center font-medium py-4'>
+        <p className='py-4 mb-4 text-blue-700'>leslieAlexander@example.com</p>
+        <div className="py-4  border-t grid grid-cols-2 divide-x "><div>Message</div>
+  <div>Profile</div>
+ 
+  </div>
+    </div>
+   </div>
+
+   <div className='w-64 shadow-md flex flex-col p-4 my-4   duration-300'>
+   <img className='w-20 mx-auto mt-[-3rem] bg-white' src={aws} alt="/" />
+
+    <h2 className=' font-bold text-center '>Leslie Alexander</h2>
+    <p className='text-center text-gray-300 font-bold'>Role</p>
+    <div className='text-center font-medium py-4'>
+        <p className='py-4 mb-4 text-blue-700'>leslieAlexander@example.com</p>
+        <div className="py-4  border-t grid grid-cols-2 divide-x "><div>Message</div>
+  <div>Profile</div>
+ 
+  </div>
+    </div>
+   </div>
+   <div className='w-64 shadow-md flex flex-col p-4 my-4   duration-300'>
+   <img className='w-20 mx-auto mt-[-3rem] bg-white' src={aws} alt="/" />
+
+    <h2 className=' font-bold text-center '>Leslie Alexander</h2>
+    <p className='text-center text-gray-300 font-bold'>Role</p>
+    <div className='text-center font-medium py-4'>
+        <p className='py-4 mb-4 text-blue-700'>leslieAlexander@example.com</p>
+        <div className="py-4  border-t grid grid-cols-2 divide-x "><div>Message</div>
+  <div>Profile</div>
+ 
+  </div>
+    </div>
+   </div>
+   <div className='w-64 shadow-md flex flex-col p-4 my-4   duration-300'>
+   <img className='w-20 mx-auto mt-[-3rem] bg-white' src={aws} alt="/" />
+
+    <h2 className=' font-bold text-center '>Leslie Alexander</h2>
+    <p className='text-center text-gray-300 font-bold'>Role</p>
+    <div className='text-center font-medium py-4'>
+        <p className='py-4 mb-4 text-blue-700'>leslieAlexander@example.com</p>
+        <div className="py-4  border-t grid grid-cols-2 divide-x "><div>Message</div>
+  <div>Profile</div>
+ 
+  </div>
+    </div>
+   </div>
+   <div className='w-64 shadow-md flex flex-col p-4 my-4   duration-300 mt-6'>
+   <img className='w-20 mx-auto mt-[-3rem] bg-white' src={aws} alt="/" />
+
+    <h2 className=' font-bold text-center '>Leslie Alexander</h2>
+    <p className='text-center text-gray-300 font-bold'>Role</p>
+    <div className='text-center font-medium py-4'>
+        <p className='py-4 mb-4 text-blue-700'>leslieAlexander@example.com</p>
+        <div className="py-4  border-t grid grid-cols-2 divide-x "><div>Message</div>
+  <div>Profile</div>
+ 
+  </div>
+    </div>
+   </div>
+   <div className='w-64 shadow-md flex flex-col p-4 my-4   duration-300 mt-6'>
+   <img className='w-20 mx-auto mt-[-3rem] bg-white' src={aws} alt="/" />
+
+    <h2 className=' font-bold text-center '>Leslie Alexander</h2>
+    <p className='text-center text-gray-300 font-bold'>Role</p>
+    <div className='text-center font-medium py-4'>
+        <p className='py-4 mb-4 text-blue-700'>leslieAlexander@example.com</p>
+        <div className="py-4  border-t grid grid-cols-2 divide-x "><div>Message</div>
+  <div>Profile</div>
+ 
+  </div>
+    </div>
+   </div>
+   <div className='w-64 shadow-md flex flex-col p-4 my-4   duration-300 mt-6'>
+   <img className='w-20 mx-auto mt-[-3rem] bg-white' src={aws} alt="/" />
+
+    <h2 className=' font-bold text-center '>Leslie Alexander</h2>
+    <p className='text-center text-gray-300 font-bold'>Role</p>
+    <div className='text-center font-medium py-4'>
+        <p className='py-4 mb-4 text-blue-700'>leslieAlexander@example.com</p>
+        <div className="py-4  border-t grid grid-cols-2 divide-x "><div>Message</div>
+  <div>Profile</div>
+ 
+  </div>
+    </div>
+   </div>
+
+   <div className='w-64 shadow-md flex flex-col p-4 my-4   duration-300 mt-6'>
+   <img className='w-20 mx-auto mt-[-3rem] bg-white' src={aws} alt="/" />
+
+    <h2 className=' font-bold text-center '>Leslie Alexander</h2>
+    <p className='text-center text-gray-300 font-bold'>Role</p>
+    <div className='text-center font-medium py-4'>
+        <p className='py-4 mb-4 text-blue-700'>leslieAlexander@example.com</p>
+        <div className="py-4  border-t grid grid-cols-2 divide-x "><div>Message</div>
+  <div>Profile</div>
+ 
+  </div>
+    </div>
+   </div>
+</div>
+
+    </div>
+  );
+};
+
+export default Cards;
