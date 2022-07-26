d92f8fed568b0355f96683da39e8da06b5914301
Anisha100 <87559560+Anisha100@users.noreply.github.com>
2022-07-26 15:40:10 +0530

    Add files via upload

---
+++ src/Components/Container.js
@@ -0,0 +1,30 @@
+import React from 'react'
+
+
+
+
+
+
+const Container = () => {
+    return (
+        <div className=" bg-gradient-to-r from-gray-100 to-gray-50 h-full " >
+            <div className="  px-8 py-1 ">
+                <p className="text-gray-500 text-lg">
+                    Elrond
+            </p>
+                <p className="font-bold text-2xl transform -translate-y-2">
+                    Hei!
+            </p>
+            </div>
+            <div className="flex   p-4 space-x-3">
+              tot
+
+            </div>
+            <div className="flex  ml-3 mt-6 space-x-6  mr-4">
+              
+            </div>
+        </div>
+    )
+}
+
+export default Container
